133d31415047e4bd7ac1f1f04fa3989fee7f25a5
Harsh R Jain <rjainharsh@gmail.com>
2021-08-26 22:52:38 +0530

    ADDED: Welcome Message

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		
+		System.out.println("=== Welcome to Address Book Program ===");
+
+	}
+
+}
