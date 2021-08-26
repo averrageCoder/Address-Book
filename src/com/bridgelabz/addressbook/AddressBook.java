d54175281208b29602fa95790d61a7c41209f02f
Harsh R Jain <rjainharsh@gmail.com>
2021-08-26 23:37:49 +0530

    ADDED: ability to create contacts

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -5,7 +5,26 @@ public class AddressBook {
 	public static void main(String[] args) {
 		
 		System.out.println("=== Welcome to Address Book Program ===");
-
+		
+		Contact contact = new Contact();
+		
+		contact.setFirstName("Adam");
+		contact.setLastName("James");
+		contact.setAddress("India");
+		contact.setCity("Bangalore");
+		contact.setState("Karnataka");
+		contact.setZipCode("56100");
+		contact.setPhoneNumber("9876543210");
+		contact.setEmail("email@email.com");
+		
+		System.out.println("\nContact Details: ");
+		System.out.println("Name: "+contact.getName());
+		System.out.println("City: "+contact.getCity());
+		System.out.println("State: "+contact.getState());
+		System.out.println("Address: "+contact.getAddress());
+		System.out.println("ZipCode: "+contact.getZipCode());
+		System.out.println("Phone Number: "+contact.getPhoneNumber());
+		System.out.println("Email: "+contact.getEmail());
 	}
 
 }
--- src/com/bridgelabz/addressbook/Contact.java
+++ src/com/bridgelabz/addressbook/Contact.java
@@ -2,4 +2,72 @@ package com.bridgelabz.addressbook;
 
 public class Contact {
 
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zipCode;
+	private String phoneNumber;
+	private String email;
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getName() {
+		return this.firstName+" "+this.lastName;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getCity() {
+		return this.city;
+	}
+	
+	public void setState(String state) {
+		this.state=state;
+	}
+	
+	public String getState() {
+		return this.state;
+	}
+	
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	public String getZipCode() {
+		return this.zipCode;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
 }
